f2d692e093fa85fe10f894d2f8a3b3fea93d16b3
John Anderson <sontek@gmail.com>
2018-10-20 19:06:35 -0400

    Add api and couchbase reference

--- api/app.py
+++ api/app.py
@@ -10,5 +10,6 @@ if __name__ == '__main__':
         config.add_route('hello', '/')
         config.add_view(hello_world, route_name='hello')
         app = config.make_wsgi_app()
+    print("http://0.0.0.0:6543")
     server = make_server('0.0.0.0', 6543, app)
     server.serve_forever()
