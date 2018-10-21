013a51aa04b492f2098086f22370a8d63148ea81
John Anderson <sontek@gmail.com>
2018-10-21 07:20:34 -0400

    Implement signin

--- api/brigaid/core/__init__.py
+++ api/brigaid/core/__init__.py
@@ -1,11 +1,11 @@
 import os
-from brigaid.auth.signup import sign_up
+from brigaid.auth.signup import sign_up, sign_in
 from pyramid.config import Configurator
 from couchbase.cluster import Cluster
 from couchbase.cluster import PasswordAuthenticator
 
 
-def configure_cb():
+def configure_cb(request):
     cluster = Cluster('couchbase://localhost')
     authenticator = PasswordAuthenticator(
         os.environ['CB_USERNAME'], os.environ['CB_PASSWORD']
@@ -18,6 +18,8 @@ def main():
     with Configurator() as config:
         config.add_route('signup', '/signup')
         config.add_view(sign_up, route_name='signup', renderer='json')
+        config.add_route('signin', '/signin')
+        config.add_view(sign_in, route_name='signin', renderer='json')
 
         config.add_request_method(configure_cb, 'couch', reify=True)
         app = config.make_wsgi_app()
