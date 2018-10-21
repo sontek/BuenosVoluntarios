313b49291fd561934b9f80a1b984f14ce70af2eb
John Anderson <sontek@gmail.com>
2018-10-20 22:39:29 -0400

    Remove code we don't need

--- web/src/js/redux_utils.js
+++ web/src/js/redux_utils.js
@@ -3,13 +3,12 @@ import rootReducer from './reducers'
 import { composeWithDevTools } from 'redux-devtools-extension/logOnlyInProduction';
 
 export const configureStore = (history) => {
-    const sagaMiddleware = createSagaMiddleware();
     const composeEnhancers = composeWithDevTools({
         // options like actionSanitizer, stateSanitizer
     });
     const store = createStore(
         rootReducer,
-        composeEnhancers(applyMiddleware(sagaMiddleware))
-);
+        //composeEnhancers(applyMiddleware())
+    );
     return store;
 };
