bcef94c95d9fb29a5aa82ee476a1a5d608762024
housewifehacker <housewifehacker@gmail.com>
2018-10-21 11:13:35 -0500

    list event

--- web/src/js/components/EventList.js
+++ web/src/js/components/EventList.js
@@ -5,34 +5,53 @@ import {
     Button,
     IconButton,
     Paper,
+    Card,
     List,
     ListItem,
     ListItemText,
     ListItemSecondaryAction,
 
 } from '@material-ui/core';
+import { Delete as DeleteIcon, Add as AddIcon } from '@material-ui/icons';
+const cardStyle = {
+    display: 'block',
+    maxWidth: '600px',
+    margin: '0 auto',
+    marginBottom: '10px',
+    marginTop: '10px',
+    padding: '10px',
+
+};
 
 class EventList extends Component {
     state = {
         loading: true,
-        events: [],
+        events: [{name: 'cool', date: '10/10/2017'}, {name: 'Mine', date: '12/1/2019'}],
+    }
+    async deleteEvent(event) {
+        console.log('deleting');
     }
     render() {
         const { classes } = this.props;
 
         return (
-                <Fragment>
+                <Card style={cardStyle}>
                 {!this.state.events.length ? <h2>No Events Scheduled</h2> : (
                         <List>
                         {this.state.events.map(event => (
                                 <ListItem>
                                 <ListItemText primary={event.name} secondary={event.date} />
+                                <ListItemSecondaryAction>
+                                <IconButton onClick={() => this.deleteEvent(event)} color="inherit">
+                                <DeleteIcon />
+                                </IconButton>
+                                                  </ListItemSecondaryAction>
                                 </ListItem>
                         )
                                               )}
                     </List>
                 ) }
-                </Fragment>
+                </Card>
         )
     }
 }
