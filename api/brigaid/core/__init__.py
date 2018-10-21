f335d228def908dfb45a728cbaece3a502600dfe
John Anderson <sontek@gmail.com>
2018-10-21 10:57:03 -0400

    Support saving interests

--- api/brigaid/core/__init__.py
+++ api/brigaid/core/__init__.py
@@ -1,9 +1,10 @@
 import os
 from brigaid.auth.signup import sign_up, sign_in
+from brigaid.interests.user_interests import update_interests
+
 from pyramid.config import Configurator
 from couchbase.cluster import Cluster
 from couchbase.cluster import PasswordAuthenticator
-
 
 def configure_cb(request):
     cluster = Cluster('couchbase://localhost')
@@ -20,6 +21,8 @@ def main():
         config.add_view(sign_up, route_name='signup', renderer='json')
         config.add_route('signin', '/signin')
         config.add_view(sign_in, route_name='signin', renderer='json')
+        config.add_route('updateUser', '/updateUser')
+        config.add_view(update_interests, route_name='updateUser', renderer='json')
 
         config.add_request_method(configure_cb, 'couch', reify=True)
         app = config.make_wsgi_app()
